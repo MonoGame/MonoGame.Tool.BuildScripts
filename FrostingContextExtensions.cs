cafc7102b1b263056426824b6cb225462328f409
Christopher Whitley <aristurtledev@gmail.com>
2024-04-29 17:11:51 -0400

    Added extension for shell execution

---
+++ FrostingContextExtensions.cs
@@ -0,0 +1,27 @@
+namespace BuildScripts;
+
+public static class FrostingContextExtensions
+{
+    private static readonly ProcessSettings _processSettings;
+
+    static FrostingContextExtensions() => _processSettings = new ProcessSettings();
+
+    public static void SetShellWorkingDir(this FrostingContext context, string path)
+    {
+        _processSettings.WorkingDirectory = path;
+    }
+
+    public static int ShellExecute(this FrostingContext context, string command, string environmentVariables = "")
+    {
+        string shellCommandPath = context switch
+        {
+            _ when context.IsRunningOnWindows() => @"C:\msys64\usr\bin\bash.exe",
+            _ when context.IsRunningOnLinux() => "sh",
+            _ when context.IsRunningOnMacOs() => "zsh",
+            _ => throw new PlatformNotSupportedException("Unsupported Platform")
+        };
+
+        _processSettings.Arguments = $"-c \"{environmentVariables} {command}\"";
+        return context.StartProcess(shellCommandPath, _processSettings);
+    }
+}
