23d2fc12a44a3493c071aa9c863ae82dfe29c693
Christopher Whitley <aristurtledev@gmail.com>
2024-04-29 22:59:44 -0400

    Added extension methods for FrostingContext

--- FrostingContextExtensions.cs
+++ FrostingContextExtensions.cs
@@ -3,10 +3,24 @@ namespace BuildScripts;
 public static class FrostingContextExtensions
 {
     private static readonly ProcessSettings _processSettings = new();
+    private static string _environmentVariables = string.Empty;
 
     public static void SetShellWorkingDir(this FrostingContext context, string path) => _processSettings.WorkingDirectory = path;
 
-    public static int ShellExecute(this FrostingContext context, string command, string environmentVariables = "")
+    public static void SetShellEnvironmentVariables(this FrostingContext context, params string[] env)
+    {
+        _environmentVariables = string.Empty;
+        for(int i = 0; i < env.Length; i+= 2)
+        {
+            string key = env[i];
+            string value = $"'{env[i + 1]}'";
+            _environmentVariables += $"export {key}={value};"; 
+        }
+    }
+
+    public static void ClearShellEnvironmentVariables(this FrostingContext context) => _environmentVariables = string.Empty;
+
+    public static int ShellExecute(this FrostingContext context, string command)
     {
         string shellCommandPath = context switch
         {
@@ -16,7 +30,8 @@ public static class FrostingContextExtensions
             _ => throw new PlatformNotSupportedException("Unsupported Platform")
         };
 
-        _processSettings.Arguments = $"-c \"{environmentVariables} {command}\"";
+        _processSettings.Arguments = $"-c \"{_environmentVariables} {command}\"";
+        context.Information($"Executing: {shellCommandPath} {string.Join(' ', _processSettings.Arguments)}");
         return context.StartProcess(shellCommandPath, _processSettings);
     }
 }
