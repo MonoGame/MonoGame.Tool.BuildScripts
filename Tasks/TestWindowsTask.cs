e11a505f91f1c2f8f278fc58c8e981a8d714f9a9
Dean Ellis <dellis1972@googlemail.com>
2026-02-11 18:31:28 +0000

    fix build error

--- Tasks/TestWindowsTask.cs
+++ Tasks/TestWindowsTask.cs
@@ -30,7 +30,7 @@ public sealed class TestWindowsTask : FrostingTask<BuildContext>
     {
         var vswhere = new VSWhereLatest(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
         var devcmdPath = vswhere.Latest(new VSWhereLatestSettings()).FullPath + @"\Common7\Tools\vsdevcmd.bat";
-        CheckDir(context, context.ArtifactsDir);
+        CheckDir(context, devcmdPath, context.ArtifactsDir);
     }
 
     private void CheckDir(BuildContext context, string devcmdPath, string dir)
